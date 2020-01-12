812089c1d0a7bc8f23422e5290dbdf394180946d
karaulovskin <karaulovskin@yandex.ru>
2020-01-12 18:30:23 +0300

    move piece

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import Game from './src/game.js'
 
 const game = new Game();
 
-console.log(game);
+window.game = game;
--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ export default class Game {
     score = 0;
     lines = 0;
     level = 0;
-    playField = [
+    playfield = [
         [0,0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,0,],
@@ -21,9 +21,10 @@ export default class Game {
         [0,0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,0,],
         [0,0,0,0,0,0,0,0,0,0,],
-        [0,0,0,0,0,0,0,0,0,0,],
-        [0,0,0,0,0,0,0,0,0,0,],
+        [1,1,0,0,0,0,0,0,0,0,],
+        [1,1,0,0,0,0,0,0,0,0,],
     ];
+
     activePiece = {
         x: 0,
         y: 0,
@@ -33,4 +34,58 @@ export default class Game {
             [0,0,0,]
         ]
     };
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
+    }
+
+    movePieceRight() {
+        this.activePiece.x += 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    }
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.y -= 1;
+        }
+    }
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
 }
